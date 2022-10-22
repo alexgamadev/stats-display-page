e62642843eae8fe210a87ac32c3cbd7e7f38c896
alex.gama <alex.gama@inseinc.com>
2022-10-22 14:37:36 +0100

    Allow for more than 2 stats displays

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import React, { useState } from 'react';
-import GlobalStats from './components/GlobalStats';
-import Leaderboard from './components/Leaderboard';
-import logo from './logo.svg';
+import { useState } from 'react';
+import GlobalStatsDisplay from './components/GlobalStatsDisplay';
+import LeaderboardDisplay from './components/LeaderboardDisplay';
+import PersonalStatsDisplay from './components/PersonalStatsDisplay';
 
 function App() {
-  const [isLeaderboard, setIsLeaderboard] = useState(true);
-  let component;
+  const [optionIndex, setOptionIndex] = useState(0);
+  let components = [
+    <LeaderboardDisplay/>,
+    <GlobalStatsDisplay/>,
+    <PersonalStatsDisplay/>
+  ];
 
-  if (isLeaderboard) {
-    component = <Leaderboard/>
-  } else {
-    component = <GlobalStats/>
-  }
+  let component = components[optionIndex];
 
   return (
-    <div className="h-screen bg-slate-900 py-6 px-6">
-      <div className="btn-group">
-        <button className={`btn ${isLeaderboard ? 'btn-active' : ''}`} onClick={() => setIsLeaderboard(true)}>Leaderboards</button>
-        <button className={`btn ${isLeaderboard ? '' : 'btn-active'}`} onClick={() => setIsLeaderboard(false)}>General Stats</button>
+    <div className="h-screen w-screen bg-base-100 py-6 px-6 flex-col items-center">
+      <header className="flex justify-center text-white font-semibold text-7xl mb-10">
+        Game Statistics
+      </header>
+      <div className="flex justify-center">
+        <div className="btn-group">
+          <button className={`btn ${optionIndex === 0 ? 'btn-active' : ''} font-bold text-primary`} onClick={() => setOptionIndex(0)}>Leaderboards</button>
+          <button className={`btn ${optionIndex === 1 ? 'btn-active' : ''} font-bold text-primary`} onClick={() => setOptionIndex(1)}>General Stats</button>
+          <button className={`btn ${optionIndex === 2 ? 'btn-active' : ''} font-bold text-primary`} onClick={() => setOptionIndex(2)}>Personal Stats</button>
+        </div>
       </div>
       {component}
     </div>
