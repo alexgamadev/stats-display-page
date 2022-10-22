30211e5f8239bd2faa6ed64ea9290776bf9e8679
alex.gama <alex.gama@inseinc.com>
2022-10-22 14:37:01 +0100

    Added personal stats display component

---
+++ src/components/PersonalStatsDisplay.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const PersonalStatsDisplay = () => {
+    return (
+        <div className="py-4 px-4 text-slate-200">
+            Personal stats - Not implemented
+        </div>
+    )
+}
+
+export default PersonalStatsDisplay;
